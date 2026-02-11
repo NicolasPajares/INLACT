50c866bbe52a3e1e3a2c7b479638599d96f67a8b
NicolasPajares <nicolas.pajares086@gmail.com>
2026-02-11 18:38:21 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const fabricas = [
   {
-    nombre: "Depósito Casa",
+    nombre: "Depósito Villa María",
     lat: -32.3830,
     lng: -63.229,
+    radio: 150
+  }
+    {
+    nombre: "Depósito Las Varillas",
+    lat: -31.8743,
+    lng: -62.7258,
     radio: 150
   }
 ];
