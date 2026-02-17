9739f63ad318756aa85536921dddeb4798df6ca4
NicolasPajares <nicolas.pajares086@gmail.com>
2026-02-17 17:13:08 -0300

    Add files via upload

---
+++ data-clientes.js
@@ -0,0 +1,16 @@
+// ===============================
+// DATA CLIENTES - INLACT
+// ===============================
+
+const clientes = [
+    {
+        id: 1,
+        nombre: "Depósito Casa",
+        zona: "Villa María"
+    },
+    {
+        id: 2,
+        nombre: "Lácteos La Manchita",
+        zona: "Oliva, Córdoba"
+    }
+];
