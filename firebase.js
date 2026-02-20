6a70f0ef84090c374cadf8dc2dab7eef0c67218c
NicolasPajares <nicolas.pajares086@gmail.com>
2026-02-19 21:48:20 -0300

    Update firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROJECT.firebaseapp.com",
+  projectId: "TU_PROJECT",
+  storageBucket: "TU_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
