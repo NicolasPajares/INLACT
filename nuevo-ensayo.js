4c839ca1460168f32649f6f168c176d6f9a2c223
NicolasPajares <nicolas.pajares086@gmail.com>
2026-06-22 18:05:28 -0300

    Update nuevo-ensayo.js

--- nuevo-ensayo.js
+++ nuevo-ensayo.js
@@ -44,6 +44,19 @@ const resultadosEl = document.getElementById("resultados");
 const conclusionEl = document.getElementById("conclusion");
 const propuestaComercialEl = document.getElementById("propuestaComercial");
 const fotosInput = document.getElementById("fotos");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  try {
+    let fotoURL = "";
+
+    // 1️⃣ subir foto
+    if (fotosInput.files.length > 0) {
+      const file = fotosInput.files[0];
+      const fotoRef = ref(storage, `ensayos/${Date.now()}_${file.name}`);
+      await uploadBytes(fotoRef, file);
+      fotoURL = await getDownloadURL(fotoRef);
+    }
 
 /**********************
  * CARGAR CLIENTES
